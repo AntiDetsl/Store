9e2c5a96cbfdfcc741fe8162f9f54e01d17f6965
Rin <Rin>
2023-06-21 04:36:48 +0400

    Created context class

---
+++ Store.DAL.Context/Migrations/20230621002646_InitialCreate.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Store.DAL.Context.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Providers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Providers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", maxLength: 7, nullable: false),
+                    ProviderId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Providers_ProviderId",
+                        column: x => x.ProviderId,
+                        principalTable: "Providers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<decimal>(type: "decimal(18,3)", precision: 18, scale: 3, nullable: false),
+                    Unit = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Items_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_OrderId",
+                table: "Items",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ProviderId",
+                table: "Orders",
+                column: "ProviderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Providers");
+        }
+    }
+}
--- Store.DAL.Context/StoreDbContext.cs
+++ Store.DAL.Context/StoreDbContext.cs
@@ -1,6 +1,31 @@
-﻿namespace Store.DAL.Context
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Entities;
+
+namespace Store.DAL.Context
 {
-    public class StoreDbContext
+    public class StoreDbContext : DbContext
     {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; } = null!;
+
+        public DbSet<OrderItem> Items { get; set; } = null!;
+
+        public DbSet<Provider> Providers { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>().Property(o => o.Number).HasColumnType("nvarchar(max)");
+
+            modelBuilder.Entity<Order>().Property(o => o.Date).HasColumnType("datetime2");
+            modelBuilder.Entity<Order>().Property(o => o.Date).HasMaxLength(7);
+
+            modelBuilder.Entity<OrderItem>().Property(o => o.Name).HasColumnType("nvarchar(max)");
+            modelBuilder.Entity<OrderItem>().Property(o => o.Unit).HasColumnType("nvarchar(max)");
+
+            modelBuilder.Entity<OrderItem>().Property(o => o.Quantity).HasPrecision(18, 3);
+
+            modelBuilder.Entity<Provider>().Property(p => p.Name).HasColumnType("nvarchar(max)");
+        }
     }
 }
