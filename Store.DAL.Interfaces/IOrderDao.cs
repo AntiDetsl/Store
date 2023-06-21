d063415dbdd48f9291d22d982e94dd525cf68b77
Rin <Rin>
2023-06-21 20:17:04 +0400

    OrderDao added

---
+++ Store.DAL.Interfaces/IOrderDao.cs
@@ -0,0 +1,17 @@
+﻿using Store.Entities;
+
+namespace Store.DAL.Interfaces
+{
+    public interface IOrderDao
+    {
+        Task<int> AddAsync(Order order);
+
+        Task DeleteAsync(int id);
+
+        Task<Order> UpdateAsync(Order order);
+
+        Task<Order> GetByIdAsync(int id);
+
+        Task<ICollection<Order>> GetAllAsync();
+    }
+}
---
+++ Store.DAL/OrderDao.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.DAL.Context;
+using Store.DAL.Interfaces;
+using Store.Entities;
+using System.Threading.Tasks;
+
+namespace Store.DAL
+{
+    public class OrderDao : IOrderDao
+    {
+        private readonly StoreDbContext _dbContext;
+
+        public OrderDao(StoreDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> AddAsync(Order order)
+        {
+            _dbContext.Add(order);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var order = await _dbContext.Orders
+                .Include(o => o.Items)
+                .SingleOrDefaultAsync(o => o.Id == id);
+
+            if(order == null) 
+            {
+                throw new ArgumentNullException(nameof(id), "Wrong ID. Order not found.");
+            }
+            
+            _dbContext.Remove(order);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<ICollection<Order>> GetAllAsync()
+        {
+            var orders = _dbContext.Orders.AsNoTracking();
+            return await orders.ToListAsync();
+        }
+
+        public async Task<Order> GetByIdAsync(int id)
+        {
+            var order = await _dbContext.Orders
+                .AsNoTracking()
+                .Include(o => o.Items)
+                .SingleAsync(o => o.Id == id);
+
+            return order;
+        }
+
+        public async Task<Order> UpdateAsync(Order order)
+        {
+            if(order == null)
+            {
+                throw new ArgumentNullException(nameof(order));
+            }
+
+            _dbContext.Update(order);
+            await _dbContext.SaveChangesAsync();
+
+            return order;
+        }
+    }
+}
